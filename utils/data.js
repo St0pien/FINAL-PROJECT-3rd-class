9647c5994467ae76c92588522584816d5512ca89
St0pien <jstepien50@gmail.com>
2021-06-12 18:52:33 +0200

    Change DB URI env var

--- utils/data.js
+++ utils/data.js
@@ -1,6 +1,6 @@
 module.exports = {
     games: [],
     SECRET: process.env.JWT_SECRET || '82c032e18e19c069f2e323b1772c18ffefff97f9d141816f4e00a26f96cd355086c024cc938854c165076c89766faa5618aed4922b3e51b3491cf9126ff0bd7e',
-    url: process.env.DB_URL || 'mongodb://172.24.128.1:27017',
+    url: process.env.MONGODB_URI || 'mongodb://172.24.128.1:27017',
     dbName: process.env.DB_NAME || 'dexter'
 }
